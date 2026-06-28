5ccdc6dda2f58337e3c07fad109e0e6d9eb54716
NISHANT7-cmd <nishantshewale69@gmail.com>
2026-06-29 00:48:55 +0530

    Change Close button variant to default

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -110,7 +110,7 @@ function DialogFooter({
       {children}
       {showCloseButton && (
         <DialogPrimitive.Close asChild>
-          <Button variant="outline">Close</Button>
+          <Button>Close</Button>
         </DialogPrimitive.Close>
       )}
     </div>
